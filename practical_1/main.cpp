668265d314df8a42546990dd570479baf0fec2e6
saltyfishhhhh <31990386+saltyfishhhhh@users.noreply.github.com>
2018-01-16 11:46:35 +0000

    1
    1

---
+++ practical_1/main.cpp
@@ -0,0 +1,21 @@
+#include <SMFL/Graphics.hpp>
+
+int main(){
+  sf::RenderWindow window(sf::VideMode(200,200),"SFML works");
+  sf::Circleshape shape(100.f):
+  shape.setFillColor(sf::Color::Green);
+
+  while (window.isOpen(){
+    sf::Event event:
+    while (window.pollEvent(event)){
+      if (event.Type == sf::Event::Closed){
+        window.close();
+      }
+    }
+    window.clear();
+    window.draw(shape);
+    window.disaplay();
+  }
+  returen 0;
+
+}
