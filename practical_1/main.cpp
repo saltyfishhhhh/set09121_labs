cb8029f6365251ca1c8b349b5ddedc9223d34dc6
mac <apple@AppledeMacBook-Pro.local>
2018-01-23 18:55:05 +0000

    11
    11

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,12 +1,12 @@
 # include <SFML/Graphics.hpp>
 
 int main(){
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!);
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
   
-   while (windows. isOpe()){
-    sf::Event event:
+   while (window. isOpen()){
+    sf::Event event;
     while (window.pollEvent(event)){
     if (event.type == sf::Event::Closed){
       window.close();
